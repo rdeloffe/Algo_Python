cd06db4589f7bd1bffc5f5fec6bbb16d2fbe07ba
Raphael DELOFFE <draphael59400@gmail.com>
2025-02-07 11:27:05 +0100

    Exercice 1 et 2

---
+++ Exo1.py
@@ -0,0 +1,15 @@
+#Echange valeur a b
+a = 1
+b = 2
+c = 3
+d = 4
+c = a
+d = b
+a = d
+b = c
+
+#ou
+
+a , b = b , a
+print("A =", a, "B =", b)
+
---
+++ Exo2.py
@@ -0,0 +1,6 @@
+# Ecrire un programme pour affichier Bonjour M. Ou Mme {Prenom} {Nom}
+
+Nom = input(str("Quelle est votre nom ? "))
+Prenom = input(str("Quelle est votre prenom ? "))
+
+print ("Bonjour M. Ou Mme", Prenom, Nom)
