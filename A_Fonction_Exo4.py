46d6f8dabcbe2c7a423fb24f98c9c7c1be1c1d3f
Raphael DELOFFE <draphael59400@gmail.com>
2025-02-11 16:21:00 +0100

    Exo 4

---
+++ A_Fonction_Exo4.py
@@ -0,0 +1,24 @@
+le_mot_if = str(input("Choisie un mot : "))
+def compter_a_if () :
+    count = 0
+    for lettre in le_mot_if :
+        if lettre == "a" :
+            count += 1
+    return count
+
+coumpter = compter_a_if()
+print(f"Dans le mot {le_mot_if} il y a {coumpter} A ")
+
+
+    
+
+
+#########################################
+
+# le_mot = str(input("Choisie un mot : "))
+
+# def compter_a () :
+#     return(le_mot.count('a'))  
+
+# count = compter_a()
+# print(f"Il y a {count} a")
