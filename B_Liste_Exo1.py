64ff725cf93c215c955367026d89d76763a24965
Raphael DELOFFE <draphael59400@gmail.com>
2025-02-12 15:18:51 +0100

    Liste Exo 1

---
+++ B_Liste_Exo1.py
@@ -0,0 +1,8 @@
+liste_chiffre = []
+for i in range (10) :
+    demande_chiffre = int(input("Donne moi un chiffre"))
+    liste_chiffre.append(demande_chiffre)
+print(liste_chiffre)
+print(liste_chiffre[-2])
+    
+    
